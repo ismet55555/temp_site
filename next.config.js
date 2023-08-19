3cf10263d7c66e507b7944b1235eee55d5be3abd
Ismet Handzic <ismet.handzic@gmail.com>
2023-08-19 15:30:11 -0400

    Initial changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Set your basePath to match your repository name
+  basePath: '/your-repo-name', // Replace with your repository name
 
-module.exports = nextConfig
+  // Optional: If you're using a custom domain, set it here
+  // assetPrefix: '/your-custom-domain', // Uncomment and replace with your custom domain
+
+  // Add your Tailwind CSS configurations here
+  // This might include postcss, plugins, and other settings
+  // Example:
+  // postcss: {
+  //   plugins: {
+  //     tailwindcss: {},
+  //     autoprefixer: {},
+  //   },
+  // },
+
+  // Any other Next.js configuration options you need
+}
+
+module.exports = nextConfig;
